2cb3116a5ab12e00e320b4c5aab46ccc5b62f79c
01Dri <drikill01@gmail.com>
2023-05-12 18:57:41 -0300

    Implementação findById inserida na class Paciente

--- src/main/java/me/dri/sistemasus/resources/SenhaResource.java
+++ src/main/java/me/dri/sistemasus/resources/SenhaResource.java
@@ -6,6 +6,7 @@ import me.dri.sistemasus.services.SenhaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +27,10 @@ public class SenhaResource {
         return ResponseEntity.ok().body(senhas);
     }
 
-
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Senha> findById(@PathVariable Integer id) {
+        Senha senha = senhaService.findById(id);
+        return ResponseEntity.ok().body(senha);
+    }
 
 }
---
+++ src/main/java/me/dri/sistemasus/services/SenhaItemService.java
@@ -0,0 +1,6 @@
+package me.dri.sistemasus.services;
+
+public class SenhaItemService {
+
+    
+}
